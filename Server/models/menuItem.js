9f6f50b218701f49252ee221f6889642094d446a
Dawit20211 <dawit.habte@students.plymouth.ac.uk>
2023-12-19 22:32:36 +0000

    menuItem and Order models added

--- Server/models/menuItem.js
+++ Server/models/menuItem.js
@@ -1,6 +1,21 @@
-import mongoose from "mongoose";
+import mongoose from 'mongoose';
 
-const productSchema = mongoose.Schema({
+const reviewSchema = mongoose.Schema({
+    name:{type:String, required: true},
+    rating:{type: Number, required: true},
+    comment:{type: String, required: true},
+    user:{
+        type: mongoose.Schema.Types.ObjectId,
+        required: true,
+        ref: 'User',
+    },
+
+},{
+    timestamps:true
+})
+
+
+const menuItemSchema = mongoose.Schema({
     
     user:{
         type: mongoose.Schema.Types.ObjectId,
@@ -19,5 +34,35 @@ const productSchema = mongoose.Schema({
         type: String,
         required: true
     },
-    
+    description:{
+        type: String,
+        required: true
+    },
+    isAvailable:{
+        type: Boolean,
+        required: true,
+        default:true
+    },
+    price:{
+        type: Number,
+        required: true,
+        default: 0
+    },
+    reviews: [reviewSchema],
+    rating: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+    numReviews: {
+      type: Number,
+      required: true,
+      default: 0,
+    },
+},{
+    timestamps: true,
 })
+
+const MenuItem = mongoose.model('MenuItem', menuItemSchema)
+
+export default MenuItem;
