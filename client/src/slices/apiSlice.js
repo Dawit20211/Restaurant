f061877a4a19b0741330b4104f15bf263c32559d
Dawit20211 <dawit.habte@students.plymouth.ac.uk>
2023-12-22 01:04:11 +0000

    check

--- client/src/slices/apiSlice.js
+++ client/src/slices/apiSlice.js
@@ -6,5 +6,5 @@ const baseQuery = fetchBaseQuery({ baseUrl: BASE_URL })
 export const apiSlice = createApi({
     baseQuery : baseQuery,
     tagTypes: ['User', 'Order', 'Menu'],
-    endpoints: (builder) => ({})
+    endpoints: (builder) => ({}),
 })
